47923928d8b34d0ba57d1ec7696afb93dd3699fa
GOATS2K <jp@glattetre.com>
2023-03-10 14:27:06 +0000

    feat: introduce utility function for generating article toc
    I've also added a .prettierrc and changed the default font

--- js/main.js
+++ js/main.js
@@ -8,61 +8,101 @@
 // Your code should be DRY, if you have repeated code, consider refactoring as a function with arguments for example.
 // We like to see what you can do. Be creative.
 
-
 // event handlers
 const scrollToBanner = () => {
   const whyBanner = document.querySelector("#why-coral");
-  whyBanner.scrollIntoView({behavior: "smooth"});
-}
+  whyBanner.scrollIntoView({ behavior: "smooth" });
+};
 
 // set event handlers on main page
 const attachEventHandlersForFrontPage = () => {
   const scrollButton = document.querySelector("#scrollButton");
-  scrollButton.addEventListener("click", scrollToBanner)
+  scrollButton.addEventListener("click", scrollToBanner);
   console.log("Attached event handlers for front page.");
-}
+};
 
 const setSectionIdsOnArticle = () => {
   const sections = document.querySelectorAll(".article-section");
-  let counter = 0;
+  let headingCounter = 0;
+  let subHeadingCounter = 0;
   for (const section of sections) {
     // get h2 and mark as section header
     for (const child of section.children) {
       if (child.tagName === "H2") {
-        counter++;
+        headingCounter++;
         child.className = `article-heading`;
-        child.id = `heading-${counter}`
+        child.id = `heading-${headingCounter}`;
+      }
+      if (child.tagName === "H3") {
+        subHeadingCounter++;
+        child.className = "article-sub-heading";
+        child.id = `sub-heading-${subHeadingCounter}`;
       }
     }
   }
-}
+};
+
+const parseSectionMap = (sectionMap) => {
+  let listItem = document.createElement("li");
+  listItem.className = "toc-list-item";
+
+  let mainHeadingLink = document.createElement("a");
+  mainHeadingLink.innerText = sectionMap.title;
+  mainHeadingLink.href = `#${sectionMap.id}`;
+  listItem.appendChild(mainHeadingLink);
+
+  for (const subHeading of sectionMap.subHeadings) {
+    let childList = document.createElement("ul");
+    childList.className = "toc-list";
+
+    let childItem = document.createElement("li");
+    childItem.className = "toc-list-item";
+
+    let link = document.createElement("a");
+    link.innerText = subHeading.title;
+    link.href = `#${subHeading.id}`;
+
+    childItem.appendChild(link);
+    childList.appendChild(childItem);
+    listItem.appendChild(childList);
+  }
+
+  return listItem;
+};
 
 const generateTableOfContentsForArticle = () => {
   setSectionIdsOnArticle();
   let tocContainer = document.querySelector(".toc");
 
   // map doesn't exist on querySelectorAll so we need to use loops
-  let mainHeadings = []
   let list = document.createElement("ul");
-  for (let section of document.querySelectorAll(".article-heading")) {
-    // get all sections with header class
-    mainHeadings.push({"id": section.id, "title": section.innerText})
-    let listItem = document.createElement("li");
-    let link = document.createElement("a");
-    link.innerText = section.innerHTML;
-    link.href = `#${section.id}`
-    listItem.appendChild(link)
+  list.className = "toc-list";
+  for (let section of document.querySelectorAll(".article-section")) {
+    const heading = section.querySelector(".article-heading");
+    let sectionMap = {
+      id: heading.id,
+      title: heading.innerText,
+      subHeadings: [],
+    };
+
+    for (const subHeading of section.querySelectorAll(".article-sub-heading")) {
+      sectionMap.subHeadings.push({
+        id: subHeading.id,
+        title: subHeading.innerText,
+      });
+    }
+
+    const listItem = parseSectionMap(sectionMap);
     list.appendChild(listItem);
   }
   tocContainer.append(list);
-  console.log(mainHeadings);
-}
+};
 
 switch (window.location.pathname) {
   case "/":
     attachEventHandlersForFrontPage();
     break;
-  case '/story.html':
+  case "/story.html":
     generateTableOfContentsForArticle();
     break;
 }
